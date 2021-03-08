d6b2fb74738df30a335287b7989f3b382e01654c
pietro <dichiarad@hotmail.com>
2021-03-08 16:33:19 -0300

    subiendo archivos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <Titulo texto="Página de About"/> 
 </template>
+<script>
+import Titulo from '@/components/Titulo'
+export default {
+    components: {
+        Titulo
+    }
+}
+</script>
+<style>
+        
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,41 @@
+<template>
+    <titulo texto="Ruta con Parámetros" />
+    <!--<button @click= "consumirAPI"> Consumir API </button> -->
+     <div v-for="item in arrayBlog" :key="item.id">
+        <router-link :to="`/blog/${item.id}`"> 
+        {{item.title}} 
+        </router-link>
+        
+    </div>  
+</template>
+<script>
+import Titulo from '../components/Titulo.vue'
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+       async consumirAPI() {
+           try {
+               const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+               const array = await data.json()
+               console.log(array)
+               this.arrayBlog = array;
+           } catch (error) {
+
+           }
+        }
+    },
+    created() {
+        this.consumirAPI()
+    }
+}
+</script>
+<style>
+    
+</style>
